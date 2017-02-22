02e72f6ea3284d4e7ffd15cef65cb7d09850fed0
Quang Dinh Luong <quangscorpio@gmail.com>
2017-02-22 16:46:11 +0700

    Initial Commit

---
+++ EnglishTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnglishTest
+//
+//  Created by QUANG on 2/22/17.
+//  Copyright © 2017 QUANG INDUSTRIES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
